a5331c16d9d900e6a8837d7a6030aa79a26f39f5
Dmitriy-Klv <dmklvd@gmail.com>
2025-12-30 19:37:49 +0100

    Add cart page methods

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,4 @@
+from pages.cart_page import CartPage
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from config.settings import settings
@@ -21,3 +22,26 @@ def test_locked_out_user(playwright_page):
     error_text = login.get_error_message()
     assert error_text == login.ERROR_MESSAGE
 
+def test_login_invalid(playwright_page):
+    login = LoginPage(playwright_page)
+
+    login.open()
+    login.perform_login("invalidlogin", "invalidpassword")
+
+    error_text = login.get_error_message()
+    assert error_text == login.ERROR_INVALID_CREDENTIALS
+
+def test_add_item_to_cart(playwright_page):
+    login = LoginPage(playwright_page)
+    inventory_page = InventoryPage(playwright_page)
+    cart_page = CartPage(playwright_page)
+
+    login.open()
+    login.perform_login(settings.test_username, settings.test_password)
+
+    inventory_page.is_opened_base_page()
+    inventory_page.add_item_to_cart()
+    inventory_page.open_cart()
+    cart_page.is_opened_cart_page()
+
+    assert "Sauce Labs Backpack" in cart_page.get_items_in_cart()
