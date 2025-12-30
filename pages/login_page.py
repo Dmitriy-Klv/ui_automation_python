bc55c3b4ed95dff22c3aeecc85cb271f372ed8d9
Dmitriy-Klv <dmklvd@gmail.com>
2025-12-30 15:22:40 +0100

    test: add locked out user login error check

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,7 +5,8 @@ class LoginPage(BasePage):
     USERNAME = "#user-name"
     PASSWORD = "#password"
     LOGIN_BTN = "#login-button"
-    ERROR_MSG = "[data-test='error']"
+    ERROR_LOCATOR = "[data-test='error']"
+    ERROR_MESSAGE = "Epic sadface: Sorry, this user has been locked out."
 
     def __init__(self, page: Page):
         super().__init__(page)
@@ -14,10 +15,11 @@ class LoginPage(BasePage):
     def open(self):
         self.page.goto("https://www.saucedemo.com")
 
-    def login(self, user: str, password: str):
+    def perform_login(self, user: str, password: str):
         self.page.fill(self.USERNAME, user)
         self.page.fill(self.PASSWORD, password)
         self.page.click(self.LOGIN_BTN)
 
     def get_error_message(self) -> str:
-        return self.page.locator(self.ERROR_MSG).inner_text()
+        return self.page.locator(self.ERROR_LOCATOR).inner_text()
+
--- tests/tests.py
+++ tests/tests.py
@@ -3,12 +3,21 @@ from pages.inventory_page import InventoryPage
 from config.settings import settings
 
 
-
-def test_login2(playwright_page):
+def test_login(playwright_page):
     login = LoginPage(playwright_page)
     inventory = InventoryPage(playwright_page)
 
     login.open()
-    login.login(settings.test_username, settings.test_password)
+    login.perform_login(settings.test_username, settings.test_password)
 
     assert inventory.is_opened()
+
+def test_locked_out_user(playwright_page):
+    login = LoginPage(playwright_page)
+
+    login.open()
+    login.perform_login("locked_out_user", settings.test_password)
+
+    error_text = login.get_error_message()
+    assert error_text == login.ERROR_MESSAGE
+
