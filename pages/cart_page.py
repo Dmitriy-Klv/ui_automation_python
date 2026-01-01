1a2c11902a10b478645fa2eef6480297b77c668e
Dmitriy-Klv <dmklvd@gmail.com>
2026-01-01 20:27:18 +0100

    add test cart price

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -16,3 +16,15 @@ class CartPage(BasePage):
 
     def get_cart_items_count(self) -> int:
         return self.page.locator(".cart_item").count()
+
+    def get_first_item_price_value(self) -> float:
+        price_text = self.page.locator(".inventory_item_price").first.inner_text()
+        return float(price_text.replace("$", ""))
+
+    def get_total_price_value(self) -> float:
+        prices = self.page.locator(".inventory_item_price")
+        return sum(
+            float(prices.nth(i).inner_text().replace("$", ""))
+            for i in range(prices.count())
+        )
+
